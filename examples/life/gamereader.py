edc3909b79f2ce794ab168e34857b7b8cc96d614
Brian Mock <mock.brian@gmail.com>
2010-11-23 12:13:03 -0800

    It works now!

--- examples/life/gamereader.py
+++ examples/life/gamereader.py
@@ -8,20 +8,26 @@ class GameReader(object):
         self.file = file
 
     def read(self):
-        f = open(file, "r")
+        f = open(self.file)
         self.lines = f.readlines()
         f.close()
 
         rows = len(self.lines)
         cols = len(self.lines[0])
-        self.map = [[self.lines[r][c]
-            for c in cols]
-            for r in rows]
+        self.map = [
+            [
+                self.lines[r][c]
+                for c in xrange(cols)
+                if not self.lines[r][c] == "\n"
+            ]
+            for r in xrange(rows)
+        ]
 
     def __str__(self):
         return str(self.map)
 
 if __name__ == "__main__":
-    g = GameReader("test.life")
+    f = "test.life"
+    g = GameReader(f)
     g.read()
     print g
