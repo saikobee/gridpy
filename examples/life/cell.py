3a240770db369935834ba71b379806630b01e7a7
Brian Mock <mock.brian@gmail.com>
2010-11-24 19:13:59 -0800

    Changed cell to not keep track of position...
    ...Which now makes it essentially a useless class

--- examples/life/cell.py
+++ examples/life/cell.py
@@ -2,24 +2,12 @@
 
 class Cell(object):
     '''This class represents a cell in the game grid'''
-
-    def __init__(self, position):
-        '''Takes the position for the cell'''
-        self.x, self.y = position
-
-    @property
-    def position(self):
-        return (self.x, self.y)
-
     def __str__(self):
-        return "#<Cell x=%i, y=%i>" % self.position
+        return "#<Cell>"
 
 if __name__ == "__main__":
-    c = Cell((0, 0))
-    d = Cell((2, 3))
-
-    c.x += 1
-    d.y -= 1
+    c = Cell()
+    d = Cell()
 
     print c
     print d
